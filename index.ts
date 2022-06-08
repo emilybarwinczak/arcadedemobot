25915a95996556fe06904f168e8098063707bed9
emilybarwinczak <92114356+emilybarwinczak@users.noreply.github.com>
2022-06-08 19:48:50 -0400

    Add files via upload

--- index.ts
+++ index.ts
@@ -22,6 +22,14 @@ client.on('messageCreate', (message) => {
 })
 
 client.on('messageCreate', (message) => {
+    if (message.content === 'hi') {
+        message.reply({
+            content: 'hello, I am the Arcade DiscordBot!',
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
     if (message.content === 'I have some questions') {
         message.reply({
             content: 'I would love to help answer some',
@@ -33,6 +41,14 @@ client.on('messageCreate', (message) => {
     if (message.content === 'howdy') {
         message.reply({
             content: 'howdy ho!',
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'what is arcade') {
+        message.reply({
+            content: 'Arcade is the leading Game-fi platform enabling holders of the $ARCADE token to earn significant rewards from multiple Play-to-Earn metaverse games without having to play the game themselves or collect the payments or NFTs. Arcade manages the entire process for our token holders, through the strategic infrastructure of guilds, players, and game developers, in-game rewards are passed onto our token holders.',
         })
     }
 })
